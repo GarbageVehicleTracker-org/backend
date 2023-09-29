1dba81e8a7a0e45d4e277ff9b0c4d801701aa39b
Dhabaldeep <deepdhabal2001@gmail.com>
2023-09-29 09:27:27 +0530

    V0.3

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const mongodb = require("mongodb");
-const cors = require("cors"); // Import the cors middleware
-const port = 5500;
+const cors = require("cors");
+
 const app = express();
+const port = 5500;
 const mongoUri =
   "mongodb+srv://garbagevehicletracker:Bodda_priyam_deep%40rahul69@garbage-vehicle-trackin.lq7esop.mongodb.net/?retryWrites=true&w=majority";
 
+// Create a global variable to store coordinates
+let currentCoordinates = {};
+
+// MongoDB client
 const client = new mongodb.MongoClient(mongoUri);
 
+// Middleware
 app.use(express.json()); // Parse JSON request bodies
-
-// Enable CORS for all routes
-app.use(cors());
+app.use(cors()); // Enable CORS for all routes
 
 // Error handling middleware
 app.use((err, req, res, next) => {
@@ -19,13 +23,36 @@ app.use((err, req, res, next) => {
   res.status(500).send("Internal server error");
 });
 
+// Route to handle incoming coordinates
+app.post("/send-coordinates", (req, res) => {
+  const { latitude, longitude } = req.body;
+  console.log(
+    `Received coordinates: Latitude ${latitude}, Longitude ${longitude}`
+  );
+
+  // Store the received coordinates
+  currentCoordinates = {
+    name: "iss",
+    id: 25544,
+    latitude,
+    longitude,
+  };
+
+  res.sendStatus(200);
+});
+
+// Route to respond to GET request with current coordinates
+app.get("/get-coordinates", (req, res) => {
+  res.json(currentCoordinates);
+});
+
 app.get("/", (req, res) => {
   console.log("Hello Linux team!");
   res.send("Welcome");
 });
+
 // Define a route for the home page
 app.get("/vehicles", async (req, res, next) => {
-  // Add 'next' parameter here
   try {
     await client.connect();
     const db = client.db("vehicles");
@@ -38,6 +65,8 @@ app.get("/vehicles", async (req, res, next) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log("Listening on port ", port);
+client.connect().then(() => {
+  app.listen(port, () => {
+    console.log("Listening on port ", port);
+  });
 });
